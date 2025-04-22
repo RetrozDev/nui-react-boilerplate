1ccd5005c191ebac6484aa00bd019901894163ac
RetrozDev <ryanbarbozapro@gmail.com>
2025-04-22 18:45:34 +0200

    Removed unused imports + refactoring

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,26 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { NuiProvider, useNuiEvent, useNuiContext } from "./hooks/useNui";
-
-function AppContent() {
+function App() {
 	const { setVisible } = useNuiContext();
 
 	useNuiEvent("toggleUi", (data) => {
 		setVisible(data.show);
 	});
-
-	return (
-		<>
-			<h1 style={{ backgroundColor: "black", color: "white" }}>
-				The ui is showed
-			</h1>
-		</>
-	);
-}
-
-function App() {
 	return (
 		<NuiProvider>
 			<AppContent />
---
+++ web/src/components/MainUi.jsx
@@ -0,0 +1,5 @@
+const MainUi = () => {
+	<h1 style={{ backgroundColor: "black", color: "white" }}>
+		The ui is showed
+	</h1>;
+};
